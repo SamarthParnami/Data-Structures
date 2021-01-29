83933785cbb553547792cfdbc159bb5f69030d5e
SamarthParnami <53820308+SamarthParnami@users.noreply.github.com>
2021-01-29 18:27:48 +0530

    Added BFS

--- Graph/Implementation of Graph.cpp
+++ Graph/Implementation of Graph.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include "List.h"
+
 using namespace std;
 
 template<typename a,typename b>
@@ -13,12 +13,66 @@ public:
         this->second=second;
     }
 };
+template<typename variable>
+class List{
+public:
+    variable val;
+    List* next=nullptr;
 
+    List(variable val,List* next=nullptr)
+    {
+        this->val=val;
+        this->next=next;
+    }
+    void pushBack(variable val)
+    {
+        List* start=this;
+        while(start->next!=nullptr)
+        {
+            start=start->next;
+        }
+        start->next=new List<variable>(val);
+    }
+    List* popFront()
+    {
+        List* nex=this->next;
+        delete this;
+        return nex;
+    }
+    void pop()
+    {
+
+        List* start=this;
+        while(start->next->next!=nullptr)
+        {
+            start=start->next;
+        }
+        delete start->next;
+        start->next=nullptr;
+    }
+    List* invertList()
+    {
+        List* head=this;
+        List* prev=nullptr;
+        while(head->next!=nullptr)
+        {
+            List* next=head->next;
+            head->next=prev;
+            prev=head;
+            head=next;
+        }
+        head->next=prev;
+        return head;
+    }
+
+
+};
 template<typename variable>
 class graphNode{
 public:
     variable val;
-    List<pairOf<graphNode<variable>*,double>>* adjacent;
+    List<pairOf<graphNode<variable>*,double>*>* adjacent;
+    bool visited;
     graphNode(variable val)
     {
         this->val=val;
@@ -26,40 +80,46 @@ public:
     }
     void addNeighbour(graphNode<variable>* neighbour,double distance)
     {
+
         if(!isAdjacentNode(neighbour))
         {
+
+            pairOf<graphNode<variable>*,double>* temp=new pairOf<graphNode<variable>*,double>(neighbour,distance);
             if(adjacent==nullptr)
             {
-                adjacent=new adjacent<pairOf<graphNode<variable>*,double>>(pairOf(neighbour,distance));
+                adjacent=new List<pairOf<graphNode<variable>*,double>*>(temp);
             }
             else{
-                adjacent.pushBack(pairOf(neighbour,distance));
+                adjacent->pushBack(temp);
             }
         }
         else{
             cout<<"It's already an adjacent node!"<<endl;
         }
+
     }
     bool isAdjacentNode(graphNode<variable>* neighbour)
     {
-        List<pairOf<graphNode<variable>*,double>> temp=adjacent;
-       
-        while(temp!=nullptr&&temp.next!=nullptr)
+        List<pairOf<graphNode<variable>*,double>*>* temp=adjacent;
+
+        while(temp!=nullptr)
         {
-            if(temp.val.first==neighbour)
+
+            if(temp->val->first==neighbour)
             {
                 return true;
             }
-            temp=temp.next;
+            temp=temp->next;
         }
-        if(temp.val.first==neighbour)
-        {
-            return true;
-        }
+
+
         return false;
     }
-    
-    
+    void setVisited(bool visited)
+    {
+        this->visited=visited;
+    }
+
 };
 template<typename variable>
 class Graph{
@@ -72,26 +132,32 @@ public:
     }
     graphNode<variable>* addNode(variable a)
     {
-        graphNode<variable>* temp=new graphNode<variable>(a)
+
+        graphNode<variable>* temp=new graphNode<variable>(a);
         nodes.push_back(temp);
         return temp;
     }
     void addDirectedEdge(variable from, variable to, double weight)
     {
+
         graphNode<variable>* node=isNodePresent(from);
         if(!node)
         {
             node=addNode(from);
         }
+
         graphNode<variable>* neighbour=isNodePresent(to);
+
         if(!neighbour)
         {
-            neighbour=addNode(to)
+            neighbour=addNode(to);
         }
-        node->addNeighbour(to,weight);
+        node->addNeighbour(neighbour,weight);
+
     }
     void addUndirectedEdge(variable from,variable to, double weight)
     {
+
         addDirectedEdge(from,to,weight);
         addDirectedEdge(to,from,weight);
     }
@@ -109,12 +175,70 @@ public:
         }
         return nullptr;
     }
-    
-};
+    vector<variable> BFS(variable from)
+    {
 
+        vector<variable> ans;
+        graphNode<variable>* start=isNodePresent(from);
+        queue<graphNode<variable>*> travel;
+        for(int i=0;i<nodes.size();i++)
+        {
+            if(nodes[i]!=nullptr)
+            {
+                nodes[i]->setVisited(false);
+            }
+        }
+
+        ans.push_back(start->val);
+        start->setVisited(true);
+        List<pairOf<graphNode<variable>*,double>*>* neighbour=start->adjacent;
+        while(neighbour!=nullptr)
+        {
+            if(!neighbour->val->first->visited)
+            {
+                ans.push_back(neighbour->val->first->val);
+                travel.push(neighbour->val->first);
+                neighbour->val->first->setVisited(true);
+            }
+            neighbour=neighbour->next;
+        }
+
+        while(!travel.empty())
+        {
+            neighbour=travel.front()->adjacent;
+            while(neighbour!=nullptr)
+            {
+                if(!neighbour->val->first->visited)
+                {
+                    ans.push_back(neighbour->val->first->val);
+                    travel.push(neighbour->val->first);
+                    neighbour->val->first->setVisited(true);
+                }
+                neighbour=neighbour->next;
+            }
+            travel.pop();
+        }
+
+        return ans;
+    }
+
+};
+void printVector(vector<int> a)
+{
+    for(int i=0;i<a.size();i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
 int main()
 {
-
+    Graph <int> taste(5);
+    taste.addUndirectedEdge(-1,44,1);
+    taste.addUndirectedEdge(-1,9,15);
+    taste.addUndirectedEdge(44,55,1);
+    taste.addUndirectedEdge(44,9,4);
+    printVector(taste.BFS(-1));
 
 
 
